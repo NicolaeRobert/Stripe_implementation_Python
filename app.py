72bf11de9b0c16ed383c0d0b4c3beaf8408dc686
NicolaeRobert <rnicolae602@gmail.com>
2025-12-07 22:47:03 +0200

    Implemented the webhook and everything is functional

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from flask import Flask, request, url_for, redirect, render_template
+from flask import Flask, request, url_for, redirect, render_template, g
 from flask_mailman import EmailMessage,Mail
 from dotenv import load_dotenv
 from pprint import pprint
+import mysql.connector
 import stripe
 import os
-import json
 
 load_dotenv()
 stripe.api_key=os.getenv("secret_key")
 endpoint_secret=os.getenv("endpoint_secret")
 
 app=Flask(__name__)
-
-mail=Mail()
 
 app.config["MAIL_SERVER"]="smtp.gmail.com"
 app.config["MAIL_PORT"]=465
@@ -21,6 +19,46 @@ app.config["MAIL_PASSWORD"]=os.getenv("MAIL_PASSWORD")
 app.config["MAIL_USE_TLS"]=False
 app.config["MAIL_USE_SSL"]=True
 app.config["MAIL_DEFAULT_SENDER"]=os.getenv("EMAIL")
+
+mail=Mail(app)
+
+def get_connection():
+    if not hasattr(g,"conn"):
+        g.conn=mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=os.getenv("database_password"),
+            database="stripe_webhook"
+        )
+    return g.conn
+
+
+def get_cursor():
+    conn=get_connection()
+    return conn.cursor()
+
+def delete_old_webhooks():
+    conn=get_connection()
+    mycursor=get_cursor()
+
+    mycursor.execute(
+        "DELETE FROM webhook_sessions WHERE session_date < DATE_SUB(NOW(), INTERVAL 7 DAY)"
+    )
+
+    conn.commit()
+
+    mycursor.close()
+
+def send_email_of_success(email):
+    email_message=EmailMessage(
+        subject="Payment comfirmation",
+        body="The payment was successfull. Thank you for choosing our service!",
+        from_email=os.getenv("EMAIL"),
+        to=[email],
+        reply_to=[os.getenv("EMAIL")]
+    )
+
+    email_message.send()
 
 @app.route("/",methods=["GET","POST"])
 def first_page():
@@ -45,14 +83,17 @@ def first_page():
 
 @app.route("/success")
 def success():
-    return "The payment was a success."
+    return render_template("success_page.html")
 
 @app.route("/error")
 def error():
-    return "The payment failed."
+    return render_template("error.html")
 
 @app.route('/webhook', methods=["POST"])
 def stripe_webhook():
+
+    delete_old_webhooks()
+
     body=request.data
     stripe_signature=request.headers.get('Stripe-Signature')
 
@@ -65,13 +106,35 @@ def stripe_webhook():
     except ValueError:
         return "Invalid payload",400
     except stripe.error.SignatureVerificationError:
-        return "Invalid signature",401
+        return "Invalid signature",400
     
 
-    pprint(event)
+    conn=get_connection()
+    mycursor=get_cursor()
+
+    mycursor.execute("SELECT id FROM webhook_sessions WHERE id=%s",(event["id"],))
+    id=mycursor.fetchone()
+
+    if id==None and event["type"]=="payment_intent.succeeded":
+        mycursor.execute(
+            "INSERT INTO webhook_sessions (id,email) VALUES (%s,%s)",
+            (event["id"],event["receip_email"])
+        )
+
+        conn.commit()
+
+        send_email_of_success(event["receip_email"])
+
+
+    mycursor.close()
 
     return "OK",200
 
+@app.teardown_appcontext
+def close_connetion(exception):
+    conn=g.pop("conn",None)
+    if conn is not None:
+        conn.close()
 
 if __name__=="__main__":
     app.run(debug=True)
